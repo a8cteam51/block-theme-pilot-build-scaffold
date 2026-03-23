774e450ba7d58586d2cdab5609a233026773fdfe
TommusRhodus <tom.rhodes@automattic.com>
2026-03-23 16:42:24 +0000

    Update composer deps, build assets, and theme styles
    Remove wpackagist/config for create-block-theme and add phpstan/extension-installer to composer.json; regenerate composer.lock with numerous dependency updates and new dev packages. Rebuild mu-plugin block assets (dummy-subscribe-form and table-of-contents), update package-lock.json, and modify theme fse-pilot styles (SCSS, CSS, RTL and source map).

--- mu-plugins/fse-pilot-blocks/build/dummy-subscribe-form/index.asset.php
+++ mu-plugins/fse-pilot-blocks/build/dummy-subscribe-form/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'fac90dfcb28f63c1062c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8d1f56ce43e01c6880c7');
--- mu-plugins/fse-pilot-blocks/build/table-of-contents/index.asset.php
+++ mu-plugins/fse-pilot-blocks/build/table-of-contents/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'dd82bc6bcc5733b3ea65');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '18604ff83c71052ea438');
